755e60df54ef2f5ea065da85ddd7ecfa58e6236c
dafnemus <dafnemus@gmail.com>
2022-05-09 17:49:13 -0300

    cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
+    # config.cache_store = :memory_store
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
@@ -24,7 +25,8 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
+    config.cache_store = :memory_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
